e0897a5a68962967a88d4e480e8a1a7ff374f500
Andrew Maguire <andrewm4894@gmail.com>
2020-05-18 17:52:56 +0100

    adding schema module

--- am4894bq/_nbdev.py
+++ am4894bq/_nbdev.py
@@ -3,7 +3,9 @@
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
 index = {"say_hello": "00_core.ipynb",
-         "get_schema": "01_schema.ipynb"}
+         "get_schema": "01_schema.ipynb",
+         "dtype_to_bqtype": "01_schema.ipynb",
+         "df_to_bq_schema": "01_schema.ipynb"}
 
 modules = ["core.py",
            "schema.py"]
